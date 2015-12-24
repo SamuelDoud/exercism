02370d7f51b17770671d1670e03f25c06681ba58
Samuel Doud <sd8396a@student.american.edu>
2015-12-24 09:19:09 -0600

    Completed robot and summing exercises

---
+++ csharp/robot-name/Robot.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+
+class Robot
+{
+    public static List<string> UsedRobotNames = new List<string>();
+    public string Name{get; private set;}
+    public Robot()
+    {
+        Name = GenrateRandomRobotName();
+    }
+    public static string GenrateRandomRobotName()
+    {
+        int i;
+        Random generator = new Random(DateTime.Now.Millisecond);//set the seed to the the milliseconds of the day.
+        char[] strCharArr = new char[5];//how long the name is to be
+        for (i = 0; i < 2; i++)//the first two characters are random alphabetic characters
+        {
+            strCharArr[i] = (char)(generator.Next(26) + 65);//there are 26 characters in the alphabet with the first ascii being at 65
+        }
+        for (; i < 5; i++ )
+        {
+            strCharArr[i] = (char)(generator.Next(10) + 48);//there are 106 characters in the digits with the first ascii being at 48
+        }
+        string newName = new string(strCharArr);
+        if (UsedRobotNames.Contains(newName))
+        {
+            newName = GenrateRandomRobotName();//the name is already in the list, recur to generate a new name
+        }
+        UsedRobotNames.Add(newName); //add the name to the list
+        return newName;//return the name
+    }
+    public void Reset()
+    {
+        Name = GenrateRandomRobotName();
+    }
+}
+
---
+++ csharp/sum-of-multiples/SumOfMultiples.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+class SumOfMultiples
+{
+    public List<int> _multiples { get; private set; }
+    public readonly List<int> defaultMultiples = new List<int>() { 3, 5 };
+    public SumOfMultiples()
+    {
+        _multiples = defaultMultiples;
+    }
+    public SumOfMultiples(List<int> multiples)
+    {
+        _multiples = multiples;
+    }
+    public int To(int limit)
+    {
+        Dictionary<int, int> multiplesUnderLimit = new Dictionary<int, int>();
+        foreach (int multiple in _multiples)
+        {
+            for (int incrementedMultiple = multiple; incrementedMultiple < limit; incrementedMultiple += multiple)
+            {
+                if (!multiplesUnderLimit.ContainsKey(incrementedMultiple))
+                {
+                    multiplesUnderLimit.Add(incrementedMultiple, 0);
+                }
+            }
+        }
+        return multiplesUnderLimit.Keys.ToList().Sum();
+    }
+    
+}
