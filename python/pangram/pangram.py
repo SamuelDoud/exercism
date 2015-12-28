ad341060ba7a5d66304756b366802a5fdf15ba00
Samuel Doud <sd8396a@student.american.edu>
2015-12-28 06:59:54 -0600

    Formatted code to be "pythonic"
    Two lines between functions and import statments

--- python/pangram/pangram.py
+++ python/pangram/pangram.py
@@ -1,5 +1,6 @@
 from collections import OrderedDict
 
+
 def is_pangram(statement):
     #iterate through the lower case string and place letters into list_letters
     list_letters = [char for char in statement.lower() if is_lower(ord(char))]
@@ -7,6 +8,8 @@ def is_pangram(statement):
     list_letters_removed_duplicates = list(OrderedDict.fromkeys(list_letters))
     #checks if the list has all the letters in it and returns that result
     return (len(list_letters_removed_duplicates) == 26)
+
+
 def is_lower(ASCII_value):
     #return if the ASCII_value is in the range of a lowerr
     return (ASCII_value >= 97 and ASCII_value <= (97 + 25))
