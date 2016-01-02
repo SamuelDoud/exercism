ced482a46cc113f09f99446c98bc10c140e8c9c4
Samuel Doud <sd8396a@student.american.edu>
2016-01-01 21:51:56 -0600

    pangram check using list comprehension and all()

--- python/pangram/pangram.py
+++ python/pangram/pangram.py
@@ -1,15 +1,7 @@
-from collections import OrderedDict
+﻿from string import ascii_lowercase
 
 
 def is_pangram(statement):
-    #iterate through the lower case string and place letters into list_letters
-    list_letters = [char for char in statement.lower() if is_lower(ord(char))]
-    #removes duplicates from list by converting to dictionary and back to list
-    list_letters_removed_duplicates = list(OrderedDict.fromkeys(list_letters))
-    #checks if the list has all the letters in it and returns that result
-    return (len(list_letters_removed_duplicates) == 26)
-
-
-def is_lower(ASCII_value):
-    #return if the ASCII_value is in the range of a lowerr
-    return (ASCII_value >= 97 and ASCII_value <= (97 + 25))
+    return all(letter in statement.lower for letter in ascii_lowercase)
+    #takes every letter in the lower case and checks for its existence
+    #in the statement, if ALL letters are satisfied, return true
